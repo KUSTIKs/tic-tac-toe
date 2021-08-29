de8e00d663d9e5a22a0a2a09f793c7474dd98b8e
Artem Khvostyk <5hv5art@gmail.com>
2021-08-29 20:36:06 +0300

    feat: implement TicTacToe class

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,70 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.winner = null;
+    this.currentPlayer = 'x';
+    this.field = [...Array(3)].map(() => Array(3).fill(null));
+    this.matches = [
+      ['0', '1', '2'],
+      ['3', '4', '5'],
+      ['6', '7', '8'],
+      ['0', '3', '6'],
+      ['1', '4', '7'],
+      ['2', '5', '8'],
+      ['0', '4', '8'],
+      ['2', '4', '6'],
+    ];
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.field[rowIndex][columnIndex]) {
+      return 'this field is not empty';
     }
-
-    getCurrentPlayerSymbol() {
-
+    this.field[rowIndex][columnIndex] = this.currentPlayer;
+    this.checkForWin();
+    if (this.noMoreTurns() && !this.winner) {
+      this.winner = null;
     }
+    this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
+  }
 
-    nextTurn(rowIndex, columnIndex) {
-
+  checkForWin() {
+    const flatten = this.field.flat();
+    for (let i = 0; i < this.matches.length; i++) {
+      let firstElem = flatten[this.matches[i][0]];
+      if (!firstElem) continue;
+      if (this.matches[i].every((ind) => flatten[ind] === firstElem)) {
+        this.winner = firstElem;
+      }
     }
+  }
 
-    isFinished() {
+  isFinished() {
+    return !!this.winner || this.noMoreTurns();
+  }
 
-    }
+  getWinner() {
+    return this.winner;
+  }
 
-    getWinner() {
+  noMoreTurns() {
+    return this.getTurnsCount() === 0;
+  }
 
-    }
+  isDraw() {
+    return this.winner === null && this.isFinished();
+  }
 
-    noMoreTurns() {
+  getTurnsCount() {
+    return this.field.flat().filter((el) => el === null).length;
+  }
 
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
